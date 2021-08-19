14165364665d9fa3ef609ea55193bd8cd97eb103
Gitthuma <george.githuma@gmail.com>
2021-08-20 00:31:29 +0300

    Add the two variables and store the result in a variable result

--- basic-calculator.py
+++ basic-calculator.py
@@ -1,3 +1,6 @@
 # create two variables that accepts user input
 num1 = input("Enter a number: ")
 num2 = input("Enter another number: ")
+
+# Add the two variables and store the result in a variable result
+result = num1 + num2
